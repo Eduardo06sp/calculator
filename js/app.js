bcd5b4d00623b3da1b1dc10358e151916cc8d1c7
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-04-20 20:42:57 -0700

    Update calculator display when clicking on numbers and validate input

--- js/app.js
+++ js/app.js
@@ -34,3 +34,44 @@ function calculate(a, b, operator) {
       break;
   }
 }
+
+let operandA = null;
+let operandB = null;
+let operator = '';
+let result = '';
+
+const calculatorNumbers = document.querySelector('.numbers');
+calculatorNumbers.addEventListener('mousedown', updateNumberDisplayed);
+
+function updateNumberDisplayed(e) {
+  let number = e.key || e.target.textContent;
+
+  if (validateNumber(number)) {
+    result += number;
+    updateDisplay(result);
+  }
+}
+
+function validateNumber(number) {
+  let integer = parseInt(number);
+
+  if (integer === 1 ||
+      integer === 2 ||
+      integer === 3 ||
+      integer === 4 ||
+      integer === 5 ||
+      integer === 6 ||
+      integer === 7 ||
+      integer === 8 ||
+      integer === 9 ||
+      integer === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateDisplay(content) {
+  const calcDisplay = document.querySelector('.display-content');
+  calcDisplay.textContent = content;
+}
